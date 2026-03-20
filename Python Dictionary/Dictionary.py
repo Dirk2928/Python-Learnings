03547b7a07226d2fbc9894b54d50c9a3b624eaa5
Dirk <cruz_dirkmaverick@plpasig.edu.ph>
2026-03-20 21:50:01 +0800

    third commit

---
+++ Python Dictionary/Dictionary.py
@@ -0,0 +1,11 @@
+# Create the recipe dictionary
+recipe = {"olive_oil": 30,
+# Add garlic
+          "garlic": 15,
+# Add tomatoes
+          "tomatoes": 400}
+
+# Add basil to the recipe dictionary
+recipe["basil"] = 20
+
+print(recipe)
---
+++ Sets and Tuples/Sets_Tuples.py
@@ -0,0 +1,11 @@
+# Create a tuple
+cup_conversion = (1,240)
+
+# Check the type
+print(type(cup_conversion))
+
+# Convert the all_ingredients list to a set
+unique_ingredients = set(all_ingredients)
+
+# Sort unique_ingredients alphabetically
+print(sorted(unique_ingredients))
